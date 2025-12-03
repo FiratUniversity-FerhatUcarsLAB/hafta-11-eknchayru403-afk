656e61ce11c0f60b49f57c474832fd8b256763f9
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-12-03 10:11:20 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,30 @@
+/**
+ * Ad Soyad: [Hayrunnisa Ekinci]
+ * Öğrenci No: [240541043]
+ * Proje: [Exercise4_2]
+ * Tarih: [03/12/2025]
+ */
 public class Exercise4_2 {
 
   // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+        zippo("rattle", 13);//1
+    }//14
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
+        System.out.println(blimp);//7
+        zippo("ping", -5);//8
+    }//13
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
-        }
+        if (flag < 0){//2,9
+            System.out.println(quince + " zoop");//10
+        } else{
+            System.out.println("ik");//3
+            baffle(quince);//4
+            System.out.println("boo-wa-ha-ha");//12
+        }//11
     }
 }
+                                                             
