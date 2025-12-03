975683793d1b0c6fa5582a465b75040424c932ca
Hayrunnisa EKİNCİ <eknchayru403@gmail.com>
2025-12-03 10:34:34 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,9 @@
+/**
+ * Ad Soyad: [Hayrunnisa Ekinci]
+ * Öğrenci No: [240541043]
+ * Proje: [Exercise4_4]
+ * Tarih: [03/12/2025]
+ */
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,9 +18,22 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        /*
+         * Cevap:
+         * Kod derlenir ve calisir. Derleyici herhangi bir hata veya uyari vermez.
+         * Dondurulen deger (42) yok sayilir ve herhangi bir etkiye neden olmaz.
+         */
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+
+        /*
+         * Cevap 2:
+         * Kod derlenirken hata verir.
+         * Hata mesaji: "error: 'void' type not allowed here" (veya benzeri).
+         * Aciklama: 'void' metotlar deger dondurmez, bu yuzden bir aritmetik ifadenin
+         * (ornegin + 7) icinde kullanilamazlar.
+         */
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
